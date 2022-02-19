9896850db9b52c1d004080537246b71e63778b0e
Byron <byron@Byrons-MacBook-Air.local>
2022-02-19 13:49:56 -0800

    add conditions

--- js/condition-builder.js
+++ js/condition-builder.js
@@ -7,11 +7,15 @@ document.getElementById('condition-submit').addEventListener('click', function()
   var option2 = "Send a reminder 24 hours after a timestamp";
   var option3 = "Send a reminder 1 week before a timestamp";
   var option4 = "Send a reminder 1 week after a timestamp";
+  var option5 = "Group people within the past 24 hours";
+  var option6 = "Group people within the next 24 hours";
   var option0Condition = "Select an option";
-  var option1Condition = "<h3>Condition 1: timestamp is between 24 hours from `now` and 48 hours from `now`</h3><ul><li>is a timestamp after a relative date of 1 day from `now`</li><li>is a timestamp before a relative date of 2 days from `now`</li></ul><h3>Condition 2: timestamp is between `now` and 24 hours from `now`</h3><ul><li>is a timestamp after a relative date of of `now`</li><li>is a timestamp before a relative date of 1 days from `now`</li></ul>";
-  var option2Condition = "<h3>Condition 1: timestamp is between 24 hours ago and 48 hours ago</h3><ul><li>is a timestamp before a relative date of 1 day ago</li><li>is a timestamp after a relative date of 2 days ago</li></ul><h3>Condition 2: timestamp is between 24 hours ago and `now`</h3><ul><li>is a timestamp before a relative date of of `now`</li><li>is a timestamp after a relative date of 1 days ago</li></ul>";
+  var option1Condition = "<h3>Condition 1: timestamp is between 24 hours from `now` and 48 hours from `now`</h3><ul><li>is a timestamp after a relative date of 1 day from `now`</li><li>is a timestamp before a relative date of 2 days from `now`</li></ul>";
+  var option2Condition = "<h3>Condition 1: timestamp is between 24 hours ago and 48 hours ago</h3><ul><li>is a timestamp before a relative date of 1 day ago</li><li>is a timestamp after a relative date of 2 days ago</li></ul>";
   var option3Condition = "<h3>Conditions:</h3><ul><li>Is a timestamp after a relative date of of 7 day from `now`</li><li>is a timestamp before a relative date of of 8 days from `now`</li></ul>";
   var option4Condition = "<h3>Conditions:</h3><ul><li>Is a timestamp before a relative date of of 7 day ago</li><li>Is a timestamp after a relative date of of 8 days ago</li></ul>";
+  var option5Condition = "<h3>Condition: timestamp is between `now` and 24 hours ago</h3><ul><li>is a timestamp before a relative date of `now`</li><li>is a timestamp after a relative date 1 days ago</li></ul>";
+  var option6Condition = "<h3>Condition: timestamp is between `now` and 24 hours from `now`</h3><ul><li>is a timestamp after a relative date of `now`</li><li>is a timestamp before a relative date 1 days from `now`</li></ul>";
   if (optionSelected === option0) {
     document.getElementById("condition-result").innerHTML += option0Condition;
     } else if (optionSelected === option1) {
@@ -20,11 +24,13 @@ document.getElementById('condition-submit').addEventListener('click', function()
       document.getElementById("condition-result").innerHTML += option2Condition;
     } else if (optionSelected === option3) {
       document.getElementById("condition-result").innerHTML += option3Condition;
-    }
-    else if (optionSelected === option4) {
+    } else if (optionSelected === option4) {
       document.getElementById("condition-result").innerHTML += option4Condition;
-    }
-      else {
+    } else if (optionSelected === option5) {
+      document.getElementById("condition-result").innerHTML += option5Condition;
+    } else if (optionSelected === option6) {
+      document.getElementById("condition-result").innerHTML += option6Condition;
+    } else {
     document.getElementById("condition-result").textContent = "";
   }
 });
